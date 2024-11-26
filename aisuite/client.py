37050c675f1f033b470a432eb78f7843f0ca9035
Sermet Pekin <sermet.pekin@gmail.com>
2024-11-27 01:29:02 +0300

    fixed mutable default argument of Client class initializer

--- aisuite/client.py
+++ aisuite/client.py
@@ -2,7 +2,7 @@ from .provider import ProviderFactory
 
 
 class Client:
-    def __init__(self, provider_configs: dict = {}):
+    def __init__(self, provider_configs: dict = None):
         """
         Initialize the client with provider configurations.
         Use the ProviderFactory to create provider instances.
@@ -22,6 +22,8 @@ class Client:
                 }
         """
         self.providers = {}
+        if provider_configs is None:
+            provider_configs = dict()
         self.provider_configs = provider_configs
         self._chat = None
         self._initialize_providers()
