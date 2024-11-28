f1d1acfb1ebb4f06e0b0ee613fc4f7d2a4019574
mkarmah <mikeyarmah@gmail.com>
2024-11-28 20:52:45 +0000

    IBM WatsonX Provider

---
+++ main.py
@@ -0,0 +1,17 @@
+import aisuite as ai
+client = ai.Client()
+
+models = ["watsonx:meta-llama/llama-3-8b-instruct"]
+
+messages = [
+    {"role": "system", "content": "Respond in Pirate English."},
+    {"role": "user", "content": "Tell me a joke."},
+]
+
+for model in models:
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=0.75
+    )
+    print(response.choices[0].message.content)
